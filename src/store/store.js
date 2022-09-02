b7736fad13f2f058966c008d489354c3f80c427e
johyomin <johyomin1119@gmail.com>
2022-09-02 15:25:29 +0900

    build 완료

--- src/store/store.js
+++ src/store/store.js
@@ -7,76 +7,17 @@ export default createStore({
 
   // state : 데이터를 모아서 관리하는 객체
   state: {
-    menuData: [
-      {
-        mainstr: '마시그래이',
-        mainlink: '#',
-        sub:[
-          {substr:'회사소개', sublink:'#'},
-          {substr:'연혁', sublink:'#'},
-          {substr:'CEO인사말', sublink:'#'},
-          {substr:'BI / 비전', sublink:'#'},
-          {substr:'오시는길', sublink:'#'}
-        ]
-      },
-      {
-        mainstr: '메뉴',
-        mainlink: '#',
-        sub:[
-          {substr:'신메뉴', sublink:'#'},
-          {substr:'커피', sublink:'#'},
-          {substr:'음료', sublink:'#'},
-          {substr:'디저트', sublink:'#'},
-          {substr:'MD상품', sublink:'#'}
-        ]
-      },
-      {        
-        mainstr: '프랜차이즈',
-        mainlink: '#',
-        sub:[
-          {substr: '가맹안내', sublink:'#'},
-          {substr: '가맹절차', sublink:'#'},
-          {substr: '매장인테리어', sublink:'#'},
-          {substr: '커피 아카데미', sublink:'#'},
-          {substr: '가맹상담신청', sublink:'#'}
-        ]
-      },
-      {       
-        mainstr: '매장안내',
-        mainlink: '#',
-        sub:[
-          {substr: '매장안내', sublink:'#'}
-        ]
-      },
-      {       
-        mainstr: '공지사항',
-        mainlink: '#',
-        sub:[
-          {substr: '공지사항', sublink:'#'},
-          {substr: '이벤트', sublink:'#'}
-        ]
-      },
-      {               
-        mainstr: '고객의 소리',
-        mainlink: '#',
-        sub:[
-          {substr: 'FAQ', sublink:'#'},
-          {substr: 'Q&amp;A', sublink:'#'},
-          {substr: '케이터링', sublink:'#'}
-        ]
-      }
-    ]
+    menuData: []
   },
 
   // actions: 서버 및 파일연동, 성공/실패 체크후 mutation 실행
   actions: {
     // 메뉴 데이터 json 로딩
     fetchMenudata({commit}){
-
       axios.get('/data/menu.json')
         .then(response => {
-          // console.log("axios:",response.data);
-          commit('MENU_DATA_INIT');
+          // console.log("axios:", response.data);
+          commit('MENU_DATA_INIT', response.data);
         })
       .catch(err => console.log(err));
     }
@@ -84,7 +25,7 @@ export default createStore({
   
   // mutations :  state(데이터) 업데이트 자리
   mutations: {
-    MENU_DATA_INIT(state,payload){
+    MENU_DATA_INIT(state, payload){
       state.menuData = payload          
     }
   },
@@ -92,8 +33,7 @@ export default createStore({
   // getters : state 를 컴포넌트에 전달, 컴포넌트의 computed 처리
   getters: {
     getMenuData(state){
-      console.log("step4: getters",state.menuData
-      )
+      // console.log("step4: getters",state.menuData)
       return state.menuData;
     }
   }
